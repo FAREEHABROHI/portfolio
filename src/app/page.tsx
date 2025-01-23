a552b27882ab7d5904bd1dc049ec854a4067cdec
fareeha <maheenbrohi88@gmail.com>
2025-01-23 18:52:09 +0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Main from "@/components/Main";
 import Skills from "@/components/Skills";
 import Projects from "@/components/Project";
 import Contact from "@/components/Contact";
-import Link from "next/link";
+
 
 export default function Home() {
   return (
--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,5 +1,4 @@
-"use client";
-import Link from "next/link";
+"use client"
 import React from "react";
 
 const Contact = () => {
